01879eb0d65fae7088219d6956496ee131aa9e52
Victor Denisov <mr.plag@gmail.com>
2019-08-23 17:06:02 +0300

    remove svg dep

---
+++ src/arrow.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Arrow = () => {
+  return (<svg width="20" height="20" viewBox="0 0 20 20">
+    <path d="M14.586 9.007l-3.293-3.293L12.707 4.3l5.707 5.707-5.707 5.707-1.414-1.414 3.293-3.293H2v-2h12.586z"/>
+  </svg>);
+};
+
+export default Arrow;
